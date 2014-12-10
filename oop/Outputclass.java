a42da979569bdb05aa118da7e75c7cec4e48872d
dynamiczoe <dynamiczoe@gmail.com>
2014-12-10 22:13:21 +0900

    사용자에게서 입력을 받기위한 공휴일 목록 출력 메소드, inputclass에 있었으나 출력 책임은 outputclass에 있다고 생각해서 메소드 이동.

--- oop/Outputclass.java
+++ oop/Outputclass.java
@@ -1,13 +1,18 @@
 package oop;
 
 public class Outputclass {
-	private int start_month, start_day, finish_month, finish_day, start_lunarMonth, start_lunarDay, finish_lunarMonth, finish_lunarDay; // 시작/종료 기념일 월,일
-	private int elapsedDay; // 경과일
+	private int start_month, start_day, finish_month, finish_day, start_lunarMonth, start_lunarDay, finish_lunarMonth, finish_lunarDay;
+	private int elapsedDay; 
 	private int isStartLunar = 0, isFinishLunar = 0;
 	private int[] recievedDate;
-	private String start_HolidayName, finish_HolidayName; // 시작/종료 기념일 이름값
+	private String start_HolidayName, finish_HolidayName; 
+	private Holidays[] array_Holidays;
+	private Holidays holidays;
+	public Outputclass(){
+		holidays = new Holidays();
+	}
 
-	void set_values(int[] date, String[] holiday_names) {
+	public void set_values(int[] date, String[] holiday_names) {
 		this.recievedDate = date;
 		start_month = recievedDate[0];
 		start_day = recievedDate[1];
@@ -16,18 +21,35 @@ public class Outputclass {
 		elapsedDay = recievedDate[4];
 		start_HolidayName = holiday_names[0];
 		finish_HolidayName = holiday_names[1];
-		if (recievedDate[5] != 0) {
+		if (isStartLunar()) {
 			isStartLunar = 1;
-			start_lunarMonth = recievedDate[5] / 100;
+			start_lunarMonth = recievedDate[5] / 100;	//메소드 추출함
 			start_lunarDay = recievedDate[5] % 100;
 		}
-		if (recievedDate[6] != 0) {
+		if (isLastLunar()) {
 			isFinishLunar = 1;
 			finish_lunarMonth = recievedDate[6] / 100;
 			finish_lunarDay = recievedDate[6] % 100;
 		}
 	}
-	void show_Result() {
+	private boolean isLastLunar() {
+		return recievedDate[6] != 0;
+	}
+	private boolean isStartLunar() {
+		return recievedDate[5] != 0;
+	}
+	public void show_List(Holidays[] array_holidays){
+		System.out.println(" 기 념 일 들 ");
+		this.array_Holidays= array_holidays;
+		for (int i = 1 ; i < holidays.get_hDay_numbers() ; i++){
+			
+			System.out.print((i) + ". " + array_Holidays[i].get_Holiday_name() + " ");
+			if (i==6||i==11||i==16){
+				System.out.print("\n");
+			}
+		}//inputclass에서 사용자에게서 입력을 받기위한 공휴일 목록 출력 메소드 
+	}
+	public void show_Result() {
 		if (isStartLunar != 0) {
 			System.out.print("시작 기념일 : " + start_HolidayName + start_lunarMonth + " 월 " + start_lunarDay + " 일   ");
 			System.out.println("        #### 양력날짜 : " + start_month + " 월 " + start_day + "일");
@@ -43,4 +65,5 @@ public class Outputclass {
 		}
 		System.out.print("경과일수 : " + elapsedDay);
 	}
+	
 }
