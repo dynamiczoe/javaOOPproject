efde3a16768bca91b4af42cc41bae004c7f8c284
dynamiczoe <dynamiczoe@gmail.com>
2014-12-10 22:03:38 +0900

    inputclass의 메소드를 포함시키고, 가독성을 높이기 위해 객체배열 생성을 메소드로 뺐다.

--- oop/Controller.java
+++ oop/Controller.java
@@ -2,44 +2,64 @@ package oop;
 
 public class Controller {
 	private String startHolidayName, finishHolidayName;
-	int[] intArray_CalculatedDate;
+	int start, finish;
+	int[] intArray_CalculatedDate, inputedNumbers;
 	String[] strArray_InputedHolidayNames = new String[2];
 	Calculateclass calculate;
 	Inputclass input;
 	Outputclass output;
 	Holidays holidays;
 	static Holidays[] array_Holidays;
-	public Controller(){
+
+	public Controller() {
 		calculate = new Calculateclass();
 		input = new Inputclass();
 		output = new Outputclass();
 		holidays = new Holidays();
 		array_Holidays = new Holidays[holidays.get_hDay_numbers()];
 	}
-	
-	void ShowDifferenceOfDaysBetwenTheseTwoHolidays(){
-		for (int i = 1 ; i < holidays.get_hDay_numbers() ; i++) {
+
+	public void setHolidays() {
+		for (int i = 1; i < holidays.get_hDay_numbers(); i++) {
 			array_Holidays[i] = new Holidays();
-			
+
 			array_Holidays[i].set_Hday_name(i);
 			array_Holidays[i].set_Hday_date(i);
 			array_Holidays[i].set_Hday_solar_or_lunar(i);
 			array_Holidays[i].set_Hday_normal_or_abnormal(i);
 		}
-		input.input_StartAndEnd(array_Holidays);
-		strArray_InputedHolidayNames = input.get_Holiday_Names(array_Holidays);
+	}
+
+	public String[] get_Holiday_Names() {
+		String holidayName[] = new String[2];
+		holidayName[0] = array_Holidays[inputedNumbers[0]].get_Holiday_name();
+		holidayName[1] = array_Holidays[inputedNumbers[1]].get_Holiday_name();
+		return holidayName;
+	}
+
+	public void ShowDifferenceOfDaysBetwenTheseTwoHolidays() {
+		setHolidays();
+		output.show_List(array_Holidays);
+		input.input_StartAndEnd();
+
+		inputedNumbers = input.get_InputedNumbers();
+		start = inputedNumbers[0];
+		finish = inputedNumbers[1];
+		strArray_InputedHolidayNames = get_Holiday_Names();
 		startHolidayName = strArray_InputedHolidayNames[0];
 		finishHolidayName = strArray_InputedHolidayNames[1];
-		int startHoliday_Solar_or_Lunar= array_Holidays[holidays.get_holidayIndex_From_Name(startHolidayName)].get_hDay_solar_or_lunar();
-		int finishHoliday_Solar_or_Lunar= array_Holidays[holidays.get_holidayIndex_From_Name(finishHolidayName)].get_hDay_solar_or_lunar();
-		
-		calculate.값_설정하기(startHolidayName, startHoliday_Solar_or_Lunar, finishHolidayName, finishHoliday_Solar_or_Lunar,array_Holidays);
+		boolean isStartHolidayLunar = array_Holidays[start].get_IsItLunar();
+		boolean isFinishHolidayLunar = array_Holidays[finish].get_IsItLunar();
+
+		calculate.set_DataToCalculate(startHolidayName, isStartHolidayLunar,
+				finishHolidayName, isFinishHolidayLunar, array_Holidays);
 		calculate.clacualte_ElapsedDays();
 		intArray_CalculatedDate = calculate.get_Date_Array();
-		
+
 		output.set_values(intArray_CalculatedDate, strArray_InputedHolidayNames);
 		output.show_Result();
 	}
+
 	public static void main(String[] args) {
 		Controller controler;
 		controler = new Controller();
