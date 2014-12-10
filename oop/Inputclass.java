ed7c60270483ebc31e53237c665b2e73608f8660
dynamiczoe <dynamiczoe@gmail.com>
2014-12-10 22:02:31 +0900

    srp위해 입력책임이 아니라고 생각되는 연산을 뺐다.

--- oop/Inputclass.java
+++ oop/Inputclass.java
@@ -6,22 +6,13 @@ public class Inputclass {
 	private int start_holiday_number, finish_holiday_number;
 	private Scanner scn;
 	private Holidays holidays;
-	private Holidays[] array_Holidays;
+	
 	public Inputclass(){
 		holidays = new Holidays();
 	}
 	
-	public void input_StartAndEnd(Holidays[] array_holidays) {
+	public void input_StartAndEnd() {
 		do {
-			System.out.println(" 기 념 일 들 ");
-			this.array_Holidays= array_holidays;
-			for (int i = 1 ; i < holidays.get_hDay_numbers() ; i++){
-				
-				System.out.print((i) + ". " + array_Holidays[i].get_Holiday_name() + " ");
-				if (i==6||i==11||i==16){
-					System.out.print("\n");
-				}
-			}
 			System.out.print("\n\n시작 기념일의 번호는? ");
 			scn = new Scanner(System.in);
 			start_holiday_number = scn.nextInt();
@@ -30,11 +21,8 @@ public class Inputclass {
 			finish_holiday_number = scn.nextInt();
 		} while (start_holiday_number > holidays.get_hDay_numbers() || finish_holiday_number > holidays.get_hDay_numbers() );
 	}
-	public String[] get_Holiday_Names(Holidays[] holidays) {	
-		this.array_Holidays=holidays;
-		String 날들[] = new String[2];
-		날들[0] = array_Holidays[start_holiday_number].get_Holiday_name();
-		날들[1] = array_Holidays[finish_holiday_number].get_Holiday_name();
-		return 날들;
-	}
+	public int[] get_InputedNumbers(){
+		int[] numbers={start_holiday_number, finish_holiday_number};
+		return numbers;
+	}//srp위해 입력책임을 제외한 연산을 뺐다.
 }
