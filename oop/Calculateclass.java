f405ed975decc71f342e03e468add0fb51f1a7d4
dynamiczoe <dynamiczoe@gmail.com>
2014-12-11 23:30:01 +0900

    경과일 계산 부분의 if else 들을  extract method해서 수정시 교정비용 줄임

--- oop/Calculateclass.java
+++ oop/Calculateclass.java
@@ -26,99 +26,131 @@ public class Calculateclass {
 	}
 
 	public void clacualte_ElapsedDays() {
-		date[5] = 0;
-		date[6] = 0;
-		int start_date = array_Holidays[holidays.get_holidayIndex_From_Name(start_HolidayName)].get_hDay_date();
-		int finish_date = array_Holidays[holidays.get_holidayIndex_From_Name(finish_HolidayName)].get_hDay_date();
-
-		if (islunarXlunar()) {
-			date[0] = start_date / 100;
-			date[1] = start_date % 100;
-			date[2] = finish_date / 100;
-			date[3] = finish_date % 100;
-			int start_date_lunaric = lunaric.calculate_ElapsedDays( date[0], date[1], year );
-			int finish_date_lunaric = lunaric.calculate_ElapsedDays( date[2], date[3], year );
-			elapsedDays = finish_date_lunaric - start_date_lunaric;
-
-			int start_lunarMonth, start_lunarDay, finish_lunarMonth, finish_lunarDay;
-			start_lunarMonth = start_date / 100;
-			start_lunarDay = start_date % 100;
-			finish_lunarMonth = finish_date / 100;
-			finish_lunarDay = finish_date % 100;
-
-			int start_Lunaric_ElapsedDay = lunaric.calculate_ElapsedDays( start_lunarMonth, start_lunarDay, year );
-			start_holiday_date = solaric.convert_LunaricElapsedDayToSolaricElapsedDay( start_Lunaric_ElapsedDay );
-			int finish_Lunaric_ElapsedDay = lunaric.calculate_ElapsedDays( finish_lunarMonth, finish_lunarDay, year );
-			finish_holiday_date = solaric.convert_LunaricElapsedDayToSolaricElapsedDay( finish_Lunaric_ElapsedDay );
-
-			date[5] = start_holiday_date;
-			date[6] = finish_holiday_date;
-		} else if (islunarXsolar()) {
-			int start_lunaricMonth, start_lunaricDay;
-			start_lunaricMonth = start_date / 100;
-			start_lunaricDay = start_date % 100;
-			date[5] = start_date;
-			int start_LunaricElapsedDay = lunaric.calculate_ElapsedDays(start_lunaricMonth, start_lunaricDay, year);
-			start_holiday_date = solaric.convert_LunaricElapsedDayToSolaricElapsedDay(start_LunaricElapsedDay);
-			date[0] = start_holiday_date / 100;
-			date[1] = start_holiday_date % 100;
-
-			date[2] = finish_date / 100;
-			date[3] = finish_date % 100;
-			int start_ElapsedDay = solaric.calculate_ElapsedDays(date[0], date[1], 0);
-			int finish_ElapsedDay = solaric.calculate_ElapsedDays(date[2], date[3], 0);
-
-			elapsedDays = finish_ElapsedDay - start_ElapsedDay;
-		} else if (issolarXlunar()) {
-			date[0] = start_date / 100;
-			date[1] = start_date % 100;
-
-			int finish_lunaricMonth, finish_lunaricDay;
-			finish_lunaricMonth = finish_date / 100;
-			finish_lunaricDay = finish_date % 100;
-			date[6] = finish_date;
-
-			int lunaric_elapsedDays = lunaric.calculate_ElapsedDays(finish_lunaricMonth, finish_lunaricDay, year);
-			finish_holiday_date = solaric.convert_LunaricElapsedDayToSolaricElapsedDay(lunaric_elapsedDays);
-			date[2] = finish_holiday_date / 100;
-			date[3] = finish_holiday_date % 100;
-			int start_ElapsedDay = solaric.calculate_ElapsedDays(0,date[0], date[1]);
-			int finish_ElapsedDay = solaric.calculate_ElapsedDays(0,date[2], date[3]);
-
-			elapsedDays = finish_ElapsedDay - start_ElapsedDay;
-		} else if (issolarXsolar()) {
-			date[0] = start_date / 100;
-			date[1] = start_date % 100;
-			date[2] = finish_date / 100;
-			date[3] = finish_date % 100;
-
-			int start_ElapsedDay = solaric.calculate_ElapsedDays(date[0], date[1], 0);
-			int finish_ElpsedDay = solaric.calculate_ElapsedDays(date[2], date[3], 0);
-
-			elapsedDays = finish_ElpsedDay - start_ElapsedDay;
+		
+		if (is_LunaXLuna()) {
+			go_LunaXLuna();
+		} else if (is_LunaXSolar()) {
+			go_LunaXSolar();
+		} else if (is_SolarXLuna()) {
+			go_SolarXLuna();
+		} else if (is_SolarXSolar()) {
+			go_SolarXSolar();
 		}
 		date[4] = elapsedDays;
 	}
 
-	private boolean islunarXlunar() {
+	private void go_SolarXSolar() {
+		date[5] = 0;
+		date[6] = 0;
+		int start_date = array_Holidays[holidays.get_holidayIndex_From_Name(start_HolidayName)].get_hDay_date();
+		int finish_date = array_Holidays[holidays.get_holidayIndex_From_Name(finish_HolidayName)].get_hDay_date();
+		
+		date[0] = start_date / 100;
+		date[1] = start_date % 100;
+		date[2] = finish_date / 100;
+		date[3] = finish_date % 100;
+
+		int start_ElapsedDay = solaric.calculate_ElapsedDays(date[0], date[1], year);
+		int finish_ElpsedDay = solaric.calculate_ElapsedDays(date[2], date[3], year);
+
+		elapsedDays = finish_ElpsedDay - start_ElapsedDay;
+	}
+
+	private void go_LunaXSolar() {
+		date[5] = 0;
+		date[6] = 0;
+		int start_date = array_Holidays[holidays.get_holidayIndex_From_Name(start_HolidayName)].get_hDay_date();
+		int finish_date = array_Holidays[holidays.get_holidayIndex_From_Name(finish_HolidayName)].get_hDay_date();
+		
+		int start_lunaricMonth, start_lunaricDay;
+		start_lunaricMonth = start_date / 100;
+		start_lunaricDay = start_date % 100;
+		date[5] = start_date;
+		int start_LunaricElapsedDay = lunaric.calculate_ElapsedDays(start_lunaricMonth, start_lunaricDay, year);
+		start_holiday_date = solaric.convert_LunaricElapsedDayToSolaricElapsedDay(start_LunaricElapsedDay);
+		date[0] = start_holiday_date / 100;
+		date[1] = start_holiday_date % 100;
+
+		date[2] = finish_date / 100;
+		date[3] = finish_date % 100;
+		int start_ElapsedDay = solaric.calculate_ElapsedDays(date[0], date[1], year);
+		int finish_ElapsedDay = solaric.calculate_ElapsedDays(date[2], date[3], year);
+
+		elapsedDays = finish_ElapsedDay - start_ElapsedDay;
+	}
+
+	private void go_LunaXLuna() {
+		date[5] = 0;
+		date[6] = 0;
+		int start_date = array_Holidays[holidays.get_holidayIndex_From_Name(start_HolidayName)].get_hDay_date();
+		int finish_date = array_Holidays[holidays.get_holidayIndex_From_Name(finish_HolidayName)].get_hDay_date();
+		
+		date[0] = start_date / 100;
+		date[1] = start_date % 100;
+		date[2] = finish_date / 100;
+		date[3] = finish_date % 100;
+		int start_date_lunaric = lunaric.calculate_ElapsedDays( date[0], date[1], year );
+		int finish_date_lunaric = lunaric.calculate_ElapsedDays( date[2], date[3], year );
+		elapsedDays = finish_date_lunaric - start_date_lunaric;
+
+		int start_lunarMonth, start_lunarDay, finish_lunarMonth, finish_lunarDay;
+		start_lunarMonth = start_date / 100;
+		start_lunarDay = start_date % 100;
+		finish_lunarMonth = finish_date / 100;
+		finish_lunarDay = finish_date % 100;
+
+		int start_Lunaric_ElapsedDay = lunaric.calculate_ElapsedDays( start_lunarMonth, start_lunarDay, year );
+		start_holiday_date = solaric.convert_LunaricElapsedDayToSolaricElapsedDay( start_Lunaric_ElapsedDay );
+		int finish_Lunaric_ElapsedDay = lunaric.calculate_ElapsedDays( finish_lunarMonth, finish_lunarDay, year );
+		finish_holiday_date = solaric.convert_LunaricElapsedDayToSolaricElapsedDay( finish_Lunaric_ElapsedDay );
+
+		date[5] = start_holiday_date;
+		date[6] = finish_holiday_date;
+	}
+
+	private void go_SolarXLuna() {
+		date[5] = 0;
+		date[6] = 0;
+		int start_date = array_Holidays[holidays.get_holidayIndex_From_Name(start_HolidayName)].get_hDay_date();
+		int finish_date = array_Holidays[holidays.get_holidayIndex_From_Name(finish_HolidayName)].get_hDay_date();
+		
+		date[0] = start_date / 100;
+		date[1] = start_date % 100;
+
+		int finish_lunaricMonth, finish_lunaricDay;
+		finish_lunaricMonth = finish_date / 100;
+		finish_lunaricDay = finish_date % 100;
+		date[6] = finish_date;
+
+		int lunaric_elapsedDays = lunaric.calculate_ElapsedDays(finish_lunaricMonth, finish_lunaricDay, year);
+		finish_holiday_date = solaric.convert_LunaricElapsedDayToSolaricElapsedDay(lunaric_elapsedDays);
+		date[2] = finish_holiday_date / 100;
+		date[3] = finish_holiday_date % 100;
+		int start_ElapsedDay = solaric.calculate_ElapsedDays(date[0], date[1], year);
+		int finish_ElapsedDay = solaric.calculate_ElapsedDays(date[2], date[3], year);
+
+		elapsedDays = finish_ElapsedDay - start_ElapsedDay;
+	}
+
+	private boolean is_LunaXLuna() {
 		if (isStartLunar == true && isFinishLunar == true) {			// 이하 유사 메소드들은 가독성이 쉽도록 extract함
 			return true;
 		} else
 			return false;
 	}
-	private boolean issolarXlunar() {
+	private boolean is_SolarXLuna() {
 		if (isStartLunar == false && isFinishLunar == true) {
 			return true;
 		} else
 			return false;
 	}
-	private boolean islunarXsolar() {
+	private boolean is_LunaXSolar() {
 		if (isStartLunar == true && isFinishLunar == false) {
 			return true;
 		} else
 			return false;
 	}
-	private boolean issolarXsolar() {
+	private boolean is_SolarXSolar() {
 		if (isStartLunar == false && isFinishLunar == false) {
 			return true;
 		} else
